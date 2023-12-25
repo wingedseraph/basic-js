7fc2555faa1ae16bd3f9ab73d84e24eca6e289ec
wingedseraph <r89647@gmail.com>
2023-12-25 11:15:03 +0300

    feat: done vigenere-cipher,what-season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,82 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    this.validateInput(message, key);
+
+    const encryptedMessage = this.cipher(message, key, "encrypt");
+    return this.reverse
+      ? encryptedMessage
+      : encryptedMessage.split("").reverse().join("");
+  }
+
+  decrypt(encryptedMessage, key) {
+    this.validateInput(encryptedMessage, key);
+
+    const decryptedMessage = this.cipher(encryptedMessage, key, "decrypt");
+    return this.reverse
+      ? decryptedMessage
+      : decryptedMessage.split("").reverse().join("");
+  }
+
+  validateInput(message, key) {
+    if (typeof message !== "string" || typeof key !== "string") {
+      throw new Error("Incorrect arguments!");
+    }
+  }
+
+  cipher(message, key, operation) {
+    const result = [];
+    const keyLength = key.length;
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i].toUpperCase();
+
+      if (char >= "A" && char <= "Z") {
+        const shift =
+          key[keyIndex].toUpperCase().charCodeAt(0) - "A".charCodeAt(0);
+        const charCode =
+          operation === "encrypt"
+            ? ((char.charCodeAt(0) + shift - "A".charCodeAt(0)) % 26) +
+              "A".charCodeAt(0)
+            : ((char.charCodeAt(0) - shift - "A".charCodeAt(0) + 26) % 26) +
+              "A".charCodeAt(0);
+
+        result.push(String.fromCharCode(charCode));
+        keyIndex = (keyIndex + 1) % keyLength;
+      } else {
+        result.push(char);
+      }
+    }
+
+    return result.join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,43 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  try {
+    if (!date) {
+      return "Unable to determine the time of year!";
+    }
+
+    if (!(date instanceof Date) || isNaN(date)) {
+      throw new Error("Invalid date!");
+    }
+
+    const month = date.getMonth();
+
+    switch (true) {
+      case month >= 2 && month <= 4:
+        return "spring";
+      case month >= 5 && month <= 7:
+        return "summer";
+      case month >= 8 && month <= 10:
+        return "autumn";
+      default:
+        return "winter";
+    }
+  } catch (error) {
+    throw new Error("Invalid date!");
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
